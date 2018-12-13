1919cdabfa44e218cad155c9ee6d15cb2b5b6672
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-13 13:33:33 -0700

    create spring boot app
    core: DevTools, Lombok
    web: Web
    sql: Jpa, H2

---
+++ src/main/java/com/galvanize/kumquatrepublic/KumquatRepublicApplication.java
@@ -0,0 +1,14 @@
+package com.galvanize.kumquatrepublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumquatRepublicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KumquatRepublicApplication.class, args);
+    }
+
+}
+
