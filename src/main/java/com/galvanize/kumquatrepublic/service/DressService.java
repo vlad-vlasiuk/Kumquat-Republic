673c351bf0f79edfeb7c3fd7a6e3cb8cee37138a
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-13 14:53:06 -0700

    add dress service

---
+++ src/main/java/com/galvanize/kumquatrepublic/service/DressService.java
@@ -0,0 +1,44 @@
+package com.galvanize.kumquatrepublic.service;
+
+import com.galvanize.kumquatrepublic.entity.Brand;
+import com.galvanize.kumquatrepublic.entity.Dress;
+import com.galvanize.kumquatrepublic.repository.BrandRepo;
+import com.galvanize.kumquatrepublic.repository.DressRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class DressService {
+
+    private final DressRepo dressRepo;
+    private final BrandRepo brandRepo;
+
+    @Autowired
+    public DressService(DressRepo dressRepo, BrandRepo brandRepo) {
+        this.dressRepo = dressRepo;
+        this.brandRepo = brandRepo;
+    }
+
+    public Iterable<Dress> findByBrand(Brand brand) {
+        return null;
+    }
+
+    public Iterable<Dress> findByColor(String color) {
+        return null;
+    }
+
+    public Iterable<Dress> findByPrice(BigDecimal priceFrom, BigDecimal priceTo) {
+        return null;
+    }
+
+    public Iterable<Dress> findByBrandAndColor(Brand brand, String color) {
+        return null;
+    }
+
+    public Iterable<Dress> findByPriceWithOrder(BigDecimal priceFrom, BigDecimal priceTo, String order) {
+        return null;
+    }
+
+}
